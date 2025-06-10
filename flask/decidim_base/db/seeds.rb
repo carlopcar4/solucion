caa36318b6d93b22e5bb39174520b070c14a7ed6
carlopcar4 <carloslopezcardenas01@gmail.com>
2025-06-10 09:06:53 +0200

    docker sin Gemfile

--- flask/decidim_base/db/seeds.rb
+++ flask/decidim_base/db/seeds.rb
@@ -6,4 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # You can remove the 'faker' gem if you do not want Decidim seeds.
-Decidim.seed!
+name = "hola"
+port = 3002
+start_date = Date.today
+end_date = start_date + 30
+id = 2
+
+puts "Creando organización: #{name}"
+
+o = Decidim::Organization.create(
+  # id: id, # El ID generalmente se autoincrementa, Decidim podría no necesitarlo en 'create'
+  host: "#{name}.localhost",
+  name: { es: "#{name}" },
+  description: {es: "Descripción para #{name}" },
+  available_locales: [:es],
+  default_locale: :es, # Como símbolo
+  reference_prefix: "hola" # <-- ¡ESTO ES CRUCIAL! Debe ser único
+)
